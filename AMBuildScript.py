8f638340dba52bdd1be0ac990041dcd81d40b8ea
dhfjidahf <78891851+454369453@users.noreply.github.com>
2026-02-09 21:05:45 +0800

    Update AMBuildScript.py

--- AMBuildScript.py
+++ AMBuildScript.py
@@ -3,6 +3,9 @@ import ambuild2
 builder = ambuild2.Builder('momsurffix_ext')
 builder.options.cxx = 'clang++'
 builder.options.arch = 'x86'
-builder.options.sdks = ['csgo']  # 调整为您的游戏
+builder.options.sdks = ['csgo']  # 或 'css' 根据您的游戏
+builder.options.mms_path = 'mmsource-1.12'  # Metamod 版本
+builder.options.sm_path = 'sourcemod-1.12'  # SourceMod 版本
 builder.AddSource('src/momsurffix_ext.cpp')
+builder.AddSource('src/extension.h')  # 添加头文件
 builder.Build()
